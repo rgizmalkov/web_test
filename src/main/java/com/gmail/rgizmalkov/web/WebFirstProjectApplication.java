21019c23f555a6a0a145187fa42ffaf1f0115017
Izmalkov Roman <rgizmalkov@gmail.com>
2017-03-20 09:59:41 +0300

    Initial commit

---
+++ src/main/java/com/gmail/rgizmalkov/web/WebFirstProjectApplication.java
@@ -0,0 +1,12 @@
+package com.gmail.rgizmalkov.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebFirstProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/com/gmail/rgizmalkov/web/WebFirstProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gmail.rgizmalkov.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebFirstProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
